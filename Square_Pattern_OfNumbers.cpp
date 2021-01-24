36e6d918941e1b943720a9a036aa6b33f9a27a77
Jay Malanker <jjmalankar@gmail.com>
2021-01-24 23:06:25 +0530

    Update Square_Pattern_OfNumbers.cpp

--- Square_Pattern_OfNumbers.cpp
+++ Square_Pattern_OfNumbers.cpp
@@ -5,10 +5,16 @@ int main()
 {
     int n, x, a, b, c, d;
     int i= 0, j= 0, k= 0, l=0;
+    string str= " ";
     cin>>n;
     cout<<"\n";
     x=(n*2)-1;
     l=x;
+
+    j= n/ 10;
+    for(i= 0; i< j; i++)
+        str= str+ "  ";                     //For an Even Pattern
+
     for(i=x;i>0;i--)
     {
         k=0;
@@ -18,17 +24,21 @@ int main()
             b=n-l+1;
             c=n-k;
             d=n+k-x+1;
-            if(a>=c)
+            if(i==x)
+            {
+                cout<<a<<"  ";
+            }
+            else if(a>=c)
                 if(a<1)
-                    cout<<b<<"  ";
+                    cout<<b<<str;
                 else
-                    cout<<a<<"  ";
+                    cout<<a<<str;
             else if(a<c)
             {
                 if(c<1)
-                    cout<<d<<"  ";
+                    cout<<d<<str;
                 else
-                    cout<<c<<"  ";
+                    cout<<c<<str;
             }
             k++;
         }
