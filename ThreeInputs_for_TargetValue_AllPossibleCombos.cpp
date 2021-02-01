b8e7a25113a9d47e86e1229648355679a3ef46f3
Jay Malanker <jjmalankar@gmail.com>
2021-02-02 00:33:24 +0530

    Create ThreeInputs_for_TargetValue_AllPossibleCombos.cpp

---
+++ ThreeInputs_for_TargetValue_AllPossibleCombos.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int No= 0, posi1= 0, posi2= 0, posi3= 0;
+    float tar= 0.0, x= 0.0, y= 0.0, z= 0.0, add= 0.0;
+    float a[999];
+    int i= 0, j= 0, k= 0;
+    cout<<"No. of Inputs?\n";   cin>>No;            //<Inputs>
+    cout<<"\nEnter your inputs\n";
+    for(i= 0; i< No; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"\nEnter your target\n";  cin>>tar;       //</Inputs>
+    for(i= 0; i< No; i++)
+    {
+        for(int j= 0; j< No; j++)
+        {
+            for(int k= 0; k< No; k++)
+            {
+                if(i==j || i==k || j==k)
+                    break;
+                x=a[i];
+                y=a[j];
+                z=a[k];
+                add= x+ y+ z;
+                if(add==tar)
+                {
+                    posi1= i;
+                    posi2= j;
+                    posi3= k;
+                    cout<<"\n["<<posi3<<", "<<posi1<<", "<<posi2<<"]";
+                }
+            }
+        }
+    }
+    return 0;
+}
