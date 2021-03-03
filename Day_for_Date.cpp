8ab4fd82b093335366cf5bc6565fd02602cbf174
Jay Malanker <jjmalankar@gmail.com>
2021-03-03 20:18:03 +0530

    Update Day_for_Date.cpp

--- Day_for_Date.cpp
+++ Day_for_Date.cpp
@@ -10,19 +10,44 @@ int Key_Value()
 {
     return 0;
 }
-void Day(int dd)
+void Day(int No)
 {
-
+    switch(No)
+    {
+        case 0:
+            cout<<"Sunday";
+        break;
+        case 1:
+            cout<<"Monday";
+        break;
+        case 2:
+            cout<<"Monday";
+        break;
+        case 3:
+            cout<<"Monday";
+        break;
+        case 4:
+            cout<<"Monday";
+        break;
+        case 5:
+            cout<<"Monday";
+        break;
+        case 6:
+            cout<<"Monday";
+        break;
+    }
 }
 int main()
 {
     int choice= 0, date= 0, month= 0, year= 0;
     cout<<"Enter Method Choice\n1. Zeller\'s Method\n2. Key-Value Method";  cin>>choice;
-    cout<<"Enter Date, Month and Year"; cin>>date>>month>>year;
+    system("cls");
+    cout<<"\nEnter Date, Month and Year\n"; cin>>date>>month>>year;
+    cout<<"\nThe Day on "<<date<<"/"<<month<<"/"<<year<<" is ";
     switch (choice)
     {
     case 1:
-        Zeller(date, month, year);
+        Day(Zeller(date, month, year));
     return 0;
     case 2:
         /*code*/
