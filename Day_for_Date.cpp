da2e80ca38afe7303d6c480b28c893c8157e555c
Jay Malanker <jjmalankar@gmail.com>
2021-03-03 20:09:25 +0530

    Create Day_for_Date.cpp

---
+++ Day_for_Date.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define cout std::cout
+#define cin std::cin
+int Zeller()
+{
+    return 0;
+}
+int Key_Value()
+{
+    return 0;
+}
+int main()
+{
+    int choice= 0, date= 0, month= 0, year= 0;
+    cout<<"Enter Method Choice\n1. Zeller\'s Method\n2. Key-Value Method";
+    return 0;
+}
