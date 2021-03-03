bc0fce693ba822f0863dd87d6696683c81c9a024
Jay Malanker <jjmalankar@gmail.com>
2021-03-03 20:09:33 +0530

    Update Day_for_Date.cpp

--- Day_for_Date.cpp
+++ Day_for_Date.cpp
@@ -12,6 +12,16 @@ int Key_Value()
 int main()
 {
     int choice= 0, date= 0, month= 0, year= 0;
-    cout<<"Enter Method Choice\n1. Zeller\'s Method\n2. Key-Value Method";
+    cout<<"Enter Method Choice\n1. Zeller\'s Method\n2. Key-Value Method";  cin>>choice;
+    cout<<"Enter Date, Month and Year"; cin>>date>>month>>year;
+    switch (choice)
+    {
+    case 1:
+        /* code */
+    break;
+    default:
+        cout<<"ERROR2";
+    break;
+    }
     return 0;
 }
